d79ac76a04b32e2130a3cb7ebd33603a1d2f4e95
Andrey Koval <andrey.koval@tecwi.ch>
2020-10-15 13:54:17 +0300

    lab5 init

---
+++ lab5/app/src/main/java/com/example/lab5/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button inverseButton;
+    Button oneColorAdjustmentButton;
+    Button splitIntoColorsButton;
+    Button combineIntoTwoImagesButton;
+    Button matrixButton;
+    Button watermarkButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        this.initViews();
+    }
+
+    private void initViews() {
+        this.inverseButton = findViewById(R.id.inverse_button);
+        this.oneColorAdjustmentButton = findViewById(R.id.one_color_adjustment_button);
+        this.splitIntoColorsButton = findViewById(R.id.split_into_three_colors_button);
+        this.combineIntoTwoImagesButton = findViewById(R.id.combine_two_images_button);
+        this.matrixButton = findViewById(R.id.matrix_button);
+        this.watermarkButton = findViewById(R.id.watermark_button);
+    }
+
+
+    public void onInverseClick(View v) {
+        Intent in = new Intent();
+        in.setClass(this, InverseImageActivity.class);
+        startActivity(in);
+    }
+
+}
